0316d85ac4760ce55dec1dd6ac7368163b0a7105
Rainer Regan <rainercoc@gmail.com>
2022-04-26 22:50:51 +0700

    First Commit

---
+++ app/src/main/java/com/rainerregan/gamelauncher/MainActivity.java
@@ -0,0 +1,19 @@
+package com.rainerregan.gamelauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchFlappyBird(View v){
+
+    }
+}
